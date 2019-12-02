217c530588fc7d6fc758ed2a9a68ab921fb41ce3
root <root@LAPTOP-SABU9FM1.localdomain>
2019-12-02 13:30:20 -0500

    combined multiple sheets of DNR walleye data, creel fish data sorted for w a

--- Walleye_PEs.R
+++ Walleye_PEs.R
@@ -31,23 +31,36 @@ VilasWal <- WALLEYE_CPE[WALLEYE_CPE$county=="VILAS",]
 #doesn't have WBIC, join by something else first 
 VilasWal2 <- creelSurvey[creelSurvey$county=="Villas",]
 
+#has species code colums in vilas wal 3, X22=walleye
 vilaswal3 <- creel_fishdata[creel_fishdata$county=="VILAS",]
+#sorting by county then by walleye species code 
+vilaswal3.0 <- vilaswal3[vilaswal3$fishSpeciesCode=="X22",]
 
+
+#joining vilaswal and vilaswal3.0(all walleye obs) by WBIC
 Walfish=VilasWal%>%
-  inner_join(vilaswal3, by="WBIC")
+  inner_join(vilaswal3.0, by="WBIC")
 
+#try to combine by species instead since vilaswal2 has no WBICs
+#have to change column species in vilaswal2 bc of capiatlization
+names(VilasWal2)[names(VilasWal2) == "Species"] <- "species"
+
+AllWAll= Walfish%>%
+  left_join(VilasWal2, by="species")
+
+#below code did not work but part of thought process 
 #sort out observations of walleye in vilaswal2 to help join
-VilasWal2.0 <- VilasWal2[VilasWal2$Species=="walleye",]
+#VilasWal2.0 <- VilasWal2[VilasWal2$Species=="walleye",]
 
 # get column names
-colnames(VilasWal2.0)
+#colnames(VilasWal2.0)
 
 # Rename column where names is "lake" to waterbody
-names(VilasWal2.0)[names(VilasWal2.0) == "lake"] <- "waterbody"
+#names(VilasWal2.0)[names(VilasWal2.0) == "lake"] <- "waterbody"
 
 #join walfish and vilaswal2.0 to put all data together, does not work
-ALLWalfish=Walfish%>%
-  left_join(VilasWal2.0, by = "waterbody")
+#ALLWalfish=Walfish%>%
+  #left_join(VilasWal2.0, by = "waterbody")
 
 
 
