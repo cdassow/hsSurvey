7a1ec7d8df05e338eb3f1fcc27badd2a109cbbf4
root <root@LAPTOP-SABU9FM1.localdomain>
2020-01-15 11:38:42 -0500

    reworking through data wranglign for walleye to narrow down WBICS

---
+++ WallData.R
@@ -0,0 +1,48 @@
+#Walleye data wrangling 
+
+#clear global evnironment
+rm(list=ls())
+
+#load any packages we'll need
+library(dplyr)
+library(ggplot2)
+
+#go to hsSurvey folder with creel data
+setwd("C:/Users/Camille/Desktop/Fishscapes/hsSurvey/")
+
+gdriveURL <- function(x){
+  x =  
+    upURL = sub("^[^=]*", "", x)
+  y1 =  "https://docs.google.com/uc?id"
+  y2 = "&export=download"
+  downURL = paste0(y1,upURL,y2)
+  read.csv(downURL, header = TRUE)
+}
+
+#get walleye pe data from dnr and load data
+walldnr<-gdriveURL("https://drive.google.com/open?id=1DPRROWv6Cf_fP6Z-kE9ZgUfdf_F_jSNT")
+
+#get creel length data and load
+creeldata<-gdriveURL("https://drive.google.com/open?id=1-d90GHr4iq_xdycFnkis1VKoYqaps00c")
+
+#get creel interview data and load
+creelindata<-gdriveURL("https://drive.google.com/open?id=1pyCKCcAQZiNZz-tX5U2QnZUc79OQEWX2")
+
+#get lake info data
+walylinfo<-gdriveURL("https://drive.google.com/open?id=1RiiiT4nmAkWyYIr7VmPk3iPCtTKlGHli")
+
+#subset data for vilas and walleye species 
+walldnrvilas<-walldnr[walldnr$county=="VILAS",]
+vilaswallPE<-walldnrvilas[walldnrvilas$species=="WALLEYE",]
+#vilaswallPE now has dnr walleye PE data for only vilas county walleye species observations
+
+creeldataV<-creeldata[creeldata$County=="VILAS",]
+creeldatawall<-creeldataV[creeldataV$Species.Code=="X22",]
+#creeldatawall now has dnr creel length data for only vilas county walleye species observations
+
+creeldataVC<-creelindata[creelindata$county=="VILAS",]
+creeldatawall2<-creeldataVC[creeldataVC$speciesCode=="X22",]
+#creeldatawall2 now has dnr creel interview data for only vilas county walleye species observations
+
+wallinfoV<-walylinfo[walylinfo$county=="Vilas",]
+wallinfo<-wallinfoV[wallinfoV$fishPresent=="Walleye"]
