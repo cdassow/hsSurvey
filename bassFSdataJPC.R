f87a1690891c7c2893f0060b7e079fafce823ab5
John Caffarelli <jcaffare@nd.edu>
2020-01-31 14:25:42 -0500

    updating the important things

--- bassFSdataJPC.R
+++ bassFSdataJPC.R
@@ -22,7 +22,7 @@ gdriveURL <- function(x){
   read.csv(downURL, header = TRUE, stringsAsFactors = FALSE)
 }
 
-#Loading BASS data from Google Drive
+###Loading BASS data from Google Drive###
 dnrBass<-gdriveURL("https://drive.google.com/open?id=11v8FbT2wnKx_CqUfxu_V9r_8fyCfcdD2")
 #shareable link for WI-DNR Bass Abundance data
 
@@ -36,10 +36,12 @@ creelFishint1VO<-gdriveURL("https://drive.google.com/open?id=1T_QeJTms9QmG65iT1u
 lakeLMB<-gdriveURL("https://drive.google.com/open?id=1FArMiTuOsoC4E08nssZUpCekSkhkMTow")
 #lake info from WI LMB lakes
 
+
+
+
 ##Organizing data##
 
-#subsetting DNR Bass information to Vilas county ### ONLY IF NEEDED
-dnrBassVilas<-dnrBass[dnrBass$county=="VILAS",]
+## ALL COUNTIES ##
 
 #removing extraneous columns from dnrBass
 dnrBass<-dnrBass[,c(1,2,3,5,12,13,27,28,29)]
@@ -48,12 +50,30 @@ dnrBass<-dnrBass[,c(1,2,3,5,12,13,27,28,29)]
 lakeLMB<-lakeLMB[,c(1,2,3,4,14)]
 
 #Joing lake info to dnrBass
+BassLinfo<-semi_join(lakeLMB,dnrBass,by="WBIC")
+BassFish=dnrBass%>%
+  inner_join(BassLinfo,by="WBIC")
+
+
+## VILAS ##
+
+#subsetting DNR Bass information to Vilas county ### ONLY IF NEEDED
+dnrBassVilas<-dnrBass[dnrBass$county=="VILAS",]
 
 #joining lake info to dnrBassVilas
 vilasBassLinfo<-semi_join(lakeLMB,dnrBassVilas,by="WBIC")
 vilasBassFish=dnrBassVilas%>%
   inner_join(vilasBassLinfo,by="WBIC")
 
+#sort to lake and year and unique values
+wbicsYear<-vilasBassFish[,c(1,2,3,5)]
+wbicsBassFishUniq<-unique(wbicsYear)#generate data frame with list of lakes which lake data is present and bass BE surveys have been conducted for.
+
+wbicsBassFishUniq<-wbicsBassFishUniq%>%
+  rename(lakeName=waterbody,county=county.x)
+
+
+#####  CREEL DATA   #####
 #subsetting LMB and SMB data from creel data
 creelLMB<-creelFishint1VO[creelFishint1VO$fishSpeciesCode=="W12",]
 creelSMB<-creelFishint1VO[creelFishint1VO$fishSpeciesCode=="W11",]
@@ -62,13 +82,58 @@ creelSMB<-creelFishint1VO[creelFishint1VO$fishSpeciesCode=="W11",]
 vilasCreelLMB<-creelLMB[creelLMB$county=="VILAS",]
 vilasCreelSMB<-creelSMB[creelSMB$county=="VILAS",]
 
-#selecting rows of creel data needed
-vilasCreelLMBsort<-vilasCreelLMB[,c(1,2,3,6,25,26,30,36,37,38)]
-vilasCreelSMBsort<-vilasCreelSMB[,c(1,2,3,6,25,26,30,36,37,38)]
+#selecting columns of creel data needed
+vilasCreelLMB<-vilasCreelLMB[,c(1,2,3,6,12,18,25,26,30,36,37,38)]
+vilasCreelSMB<-vilasCreelSMB[,c(1,2,3,6,12,18,25,26,30,36,37,38)]
 
-#sort to lake and year and unique values
-wbicsYear<-vilasBassFish[,c(1,2,3,5)]
-wbicsBassFishUniq<-unique(wbicsYear)#generate data frame with list of lakes which lake data is present and bass BE surveys have been conducted for.
+#remove NA's
+vilasCreelLMB<-vilasCreelLMB[is.na(vilasCreelLMB$WBIC)==FALSE,]
+vilasCreelSMB<-vilasCreelSMB[is.na(vilasCreelSMB$WBIC)==FALSE,]
+
+#selecting for rows with by fishedPerct
+vilasCreelLMB<-vilasCreelLMB[vilasCreelLMB$fishedPerc!=0,]
+vilasCreelSMB<-vilasCreelSMB[vilasCreelSMB$fishedPerc!=0,]
+
+#reformating Date Columns
+
+
+#adding 0 character to 3-digit times
+for (i in 1:length(vilasCreelLMB$timeStart)) {
+  if (nchar(vilasCreelLMB$timeStart[i])<4){
+    vilasCreelLMB$timeStart[i]<-paste("0",vilasCreelLMB$timeStart[i],sep = "")
+  }
+}
+
+for (i in 1:length(vilasCreelLMB$timeEnd)) {
+  if (nchar(vilasCreelLMB$timeEnd[i])<4){
+    vilasCreelLMB$timeEnd[i]<-paste("0",vilasCreelLMB$timeEnd[i],sep = "")
+  }
+}
+
+#create dateSet Column
+vilasCreelLMB$dateSet<-paste(vilasCreelLMB$dateSample,vilasCreelLMB$timeStart)
+vilasCreelLMB$dateSet<-as.POSIXct(vilasCreelLMB$dateSet,format="%m/%d/%y %H%M")
+
+#create dateSample Column
+vilasCreelLMB$dateSample<-paste(vilasCreelLMB$dateSample,vilasCreelLMB$timeEnd)
+vilasCreelLMB$dateSample<-as.POSIXct(vilasCreelLMB$dateSample,format="%m/%d/%y %H%M")
+
+#modifying notFishAmt to time
+for (i in 1:length(vilasCreelLMB$notFishingAmt)) {
+  vilasCreelLMB$notFishingAmt[i]<-(vilasCreelLMB$notFishingAmt[i]/100)
+}
+
+vilasCreelLMB$notFishingAmt<-as.character(vilasCreelLMB$notFishingAmt)
+
+for (i in 1:length(vilasCreelLMB$notFishingAmt)){
+  vilasCreelLMB$notFishingAmt[i]<-strsplit(vilasCreelLMB$notFishingAmt[i],"/.")
+}
+
+?strsplit
+
+#calculating effort
+vilasCreelLMB$effort<-difftime(vilasCreelLMB$dateSample,vilasCreelLMB$dateSet,units = "hours","minutes")
+
 
 #repeating the wbics sort for Creel data
 #LMB
@@ -80,12 +145,13 @@ wbicsYearCreelSMB<-vilasCreelSMBsort[,c(1,2,3,4)]
 wbicsCreelSMBUniq<-unique(wbicsYearCreelSMB)
 
 #renaming the surveyYear column to surveyYearCreel and surveyYearBE
-wbicsBassFishUniq<-wbicsBassFishUniq%>%
-  rename(lakeName=waterbody,county=county.x)
 wbicsCreelLMBUniq<-wbicsCreelLMBUniq%>%
   rename(surveyYearCreel=surveyYear)
 wbicsCreelSMBUniq<-wbicsCreelSMBUniq%>%
   rename(surveyYearCreel=surveyYear)
+
+
+
 
 #joining creel surveys to list of years where both SMB and LMB surveys occured
 vilasCreelWBICS=inner_join(wbicsCreelLMBUniq,wbicsCreelSMBUniq,by=c("WBIC","county","lakeName","surveyYearCreel"))
