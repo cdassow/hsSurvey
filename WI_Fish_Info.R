27c78b97d443b16332f2c2f226d197b5a24b1ab0
camillemosley <56081247+camillemosley@users.noreply.github.com>
2021-03-08 12:22:15 -0500

    anovas added

--- WI_Fish_Info.R
+++ WI_Fish_Info.R
@@ -2,14 +2,16 @@
 #loading in CSV from Weber book, need to clean up habitat and prey item variables
 Fish<-read.csv("Fishes of WI_Becker_data.csv")
 
+nullfit=lm(Fish$beta~1)
+
 summary(Fish_length<-lm(Fish$beta~Fish$max.length.mm., Fish)) #p 0.058 .
-anova(Fish_length)
+anova(Fish_length, nullfit)
 
 summary(Fish_length2<-lm(Fish$beta~Fish$min.length.mm., Fish)) # p 0.010 **
-anova(Fish_length2)
+anova(Fish_length2,nullfit)
 
 summary(Fish_spawn_time<-lm(Fish$beta~Fish$spawn.start, Fish)) #spawn start month : april p 0.020 *, may p 0.025 *
-anova(Fish_spawn_time) #0.03312
+anova(Fish_spawn_time,nullfit) #0.03312
 
 summary(Fish_spawn_end<-lm(Fish$beta~Fish$spawn.end, Fish)) #nope
 
@@ -18,7 +20,7 @@ summary(Fish_min_temp<-lm(Fish$beta~Fish$min.preffered.water.temp..Celsius.)) #n
 summary(Fish_max_temp<-lm(Fish$beta~Fish$max.preffered.water.temp..Celsius.)) #nope
 
 summary(Fish_YOY<-lm(Fish$beta~Fish$YOY.average.size..mm.)) #0.0219
-anova(Fish_YOY)
+anova(Fish_YOY,nullfit)
 
 summary(Fish_hatch<-lm(Fish$beta~Fish$hatching.time.average..days.)) #nope
 
